31c6de9912a9b550db71e52b7dd49f5e72f2741b
diegodominguez3 <diegodominguez333@gmail.com>
2016-05-04 13:51:38 -0600

    Horario post

---
+++ Horario/Horario.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Horario.WebUI.MvcApplication" Language="C#" %>
---
+++ Horario/Horario.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using Ninject;
+
+namespace EgelTraining.WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+            //tip: poner el raton sobre cada método invocado, dice qué hace
+        }
+
+        private void AddBindings()
+        {
+            //////////////aquí volveremos a cada rato
+        }
+
+
+    }
+}
